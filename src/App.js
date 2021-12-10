4ec8fbecce7f828acffff0731c70196bc1ce7412
Winifred Lu <winnie.lu@comcast.net>
2021-12-09 22:57:01 -0500

    characters inserted into state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { setupWorker, rest } from 'msw';
 import './App.css';
 
 const App = () => {
@@ -8,12 +10,27 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setCharacters] = useState([]);
+
+  // this effect happens after first render
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then(response => {
+      console.log('response', response.data);
+      // set list of characters into state
+      setCharacters(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+    })
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
     </div>
   );
+  
 }
 
 export default App;
